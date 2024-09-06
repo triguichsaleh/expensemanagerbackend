d98bcc41a7c39f60915ccd02517126187527b5a0
“triguichsaleh” <Salehtriguich@gmail.com>
2024-09-06 21:51:00 +0100

    first commit

---
+++ src/main/java/com/budgetmanager/expensemamangerbackend/ExpensemamangerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.budgetmanager.expensemamangerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensemamangerbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensemamangerbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/budgetmanager/expensemamangerbackend/ExpensemamangerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetmanager.expensemamangerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemamangerbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
